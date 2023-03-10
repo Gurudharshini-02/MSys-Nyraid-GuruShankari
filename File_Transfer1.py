00af4bc5b99bd55e3498406b94676dee6cead4ce
Guru Shankari S <84394200+Gurudharshini-02@users.noreply.github.com>
2023-03-10 14:33:31 +0530

    Add files via upload

---
+++ File_Transfer1.py
@@ -0,0 +1,27 @@
+import paramiko
+
+
+src = 'D:/Python/file1.py'
+dest= 'E:/Python_Practice'
+
+
+hostname = 'LAPTOP-RUCEIN5J'
+username = 'guru'
+password = 'kiruthiga'
+
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+
+ssh.connect(hostname=hostname, username=username, password=password)
+
+
+sftp = ssh.open_sftp()
+
+
+sftp.get(src, dest)
+
+sftp.close()
+ssh.close()
+
